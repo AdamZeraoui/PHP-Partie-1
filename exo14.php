2eaf4d435efa9625ca0b8ef21613711cbb5b8c2d
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-01-22 14:34:42 +0100

    exo 14

--- exo14.php
+++ exo14.php
@@ -1,1 +1,10 @@
 <h1>Exercice 14</h1>
+
+<p>Calculer l'âge d'une personne à partir de sa date de naissance (en années, mois, jours)</p>
+
+<h2> Résultat </h2>
+
+<?php
+
+$jour=date_time_set();
+echo $jour;
